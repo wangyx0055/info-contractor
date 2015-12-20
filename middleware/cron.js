8bdb0d628406d0c21c07e74587d8c1d07c791d01
boxizen <boxizen@gmail.com>
2015-12-20 20:28:15 +0800

    [UPD] 更新定时任务时间间隔

--- middleware/cron.js
+++ middleware/cron.js
@@ -2,7 +2,7 @@
  * @Author: boxizen
  * @Date:   2015-12-05 00:20:54
  * @Last Modified by:   boxizen
- * @Last Modified time: 2015-12-20 17:35:44
+ * @Last Modified time: 2015-12-20 20:27:43
  */
 
 'use strict';
@@ -15,7 +15,7 @@ var Schedule = require('node-schedule'),
 function cronJob() {
     var rule = new Schedule.RecurrenceRule(),
         time = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60];
-    rule.minute = time;
+    rule.second = time;
 
     Schedule.scheduleJob(rule, function() {
         Entry.active(function(res) {
